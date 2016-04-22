68a38dc94df645ef88eb05888a4b69143ad3acd9
marc jacob <mdt88551@stud.uni-stuttgart.de>
2016-04-22 15:58:37 +0200

    getSTATESv2

--- f_startShortenSO.m
+++ f_startShortenSO.m
@@ -5,12 +5,12 @@ cd ..
 
 switch data
     case 1
-        disp(strcat('SO_c',num2str(data),'_N_', num2str(isNoise)));
-        [signal,force,forces]=myShorten(data,isNoise,in,0);
+        disp(strcat('SO_c',num2str(data),'_N_', num2str(isNoise),'+STATES'));
+        [signal,force,forces, tmp_t, y, tmp_sec, tmp_x]=myShorten(data,isNoise,in,0);
         save(strcat('MA_SO_pPS_Noise',num2str(isNoise),'_c',num2str(data),'_',num2str(in),'.mat'));
     case 106
-        disp(strcat('SO_c',num2str(data),'_N_', num2str(isNoise)));
-        [signal,force,forces]=myShorten(data,isNoise,in,0);
+        disp(strcat('SO_c',num2str(data),'_N_', num2str(isNoise),'+STATES'));
+        [signal,force,forces, tmp_t, y, tmp_sec, tmp_x]=myShorten(data,isNoise,in,0);
         save(strcat('MA_SO_pPS_Noise',num2str(isNoise),'_c',num2str(data),'_',num2str(in),'.mat'));
     %{
     otherwise
@@ -22,7 +22,7 @@ switch data
         
 end
 
-function [signal,force,forces]= myShorten(Data, isNoise, maxStim, minStim)
+function [signal,force,forces, tmp_t, y, tmp_sec, tmp_x]= myShorten(Data, isNoise, maxStim, minStim)
     signal=f_getSignal(Data,minStim);
     signal.Time = signal.Time*1000;
     signal.totalTime=signal.totalTime*1000;
